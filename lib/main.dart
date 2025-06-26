99a63a0c163658505d21e880d0c12ac25c507d8c
Prototype v1 <106340187+Prototypev1@users.noreply.github.com>
2025-06-26 15:22:08 +0200

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyHomePage extends StatefulWidget {
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // always marked "final".h
 
   final String title;
 
@@ -105,10 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),

80b5e8287bce3538e5e946d8979d8e1ab3019e6a
Prototype v1 <106340187+Prototypev1@users.noreply.github.com>
2025-07-07 18:43:55 +0200

    setup cubit

--- lib/feature/main_page_container/presentation/main_page_container.dart
+++ lib/feature/main_page_container/presentation/main_page_container.dart
@@ -1,23 +1,46 @@
 import 'package:collider_loading/core/presentation/common/widgets/app_bar/custom_app_bar.dart';
+import 'package:collider_loading/di/di.dart';
+import 'package:collider_loading/feature/main_page_container/domain/cubit/main_page_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MainPageContainer extends StatelessWidget {
+class MainPageContainer extends StatefulWidget {
   final Widget child;
   const MainPageContainer({super.key, required this.child});
 
   @override
+  State<MainPageContainer> createState() => _MainPageContainerState();
+}
+
+class _MainPageContainerState extends State<MainPageContainer> {
+  late final MainPageCubit _cubit;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: CustomAppBar(),
-          ),
-        ],
+    return BlocProvider(
+      create: (context) {
+        _cubit = MainPageCubit();
+        return _cubit;
+      },
+      child: BlocConsumer(
+        listener: (context, state) {
+          //might need some logic
+        },
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            body: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: CustomAppBar(),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
